43995c74b6509382d2d52b06a1d29e949ea51544
stanly08 <anasistanly@gmail.com>
2024-04-28 14:52:00 -0700

    views

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,24 @@
-#!/usr/bin/python3
-"""index.py to connect to API"""
-from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+#!/usr/bin/python3xx
+'''api status'''
+import models
 from models import storage
-
-
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+from models.base_model import BaseModel
+from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
 
 
 @app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+def stuff():
+    '''JSON Responses'''
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
